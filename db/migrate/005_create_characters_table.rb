5b0a6e90c0eeb121fda6c876adecd387ca4b7e8f
Christine Cua <cloudycua@gmail.com>
2018-06-08 18:03:48 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters_table.rb
+++ db/migrate/005_create_characters_table.rb
@@ -1,7 +1,7 @@
 class CreateCharactersTable < ActiveRecord::Migration[4.2]
-    create_table :characters do |t|
-      t.string  :name
-      t.integer :show_id
-    end
+  create_table :characters do |t|
+    t.string  :name
+    t.integer :show_id
   end
 end
+
