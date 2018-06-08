5b5e33894e87657aeda0283dfbd7a9c767d68607
Christine Cua <cloudycua@gmail.com>
2018-06-08 18:20:35 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,6 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.collect {|c| "#{c.name} - #{c.show_id.name}"}"
+    self.characters.collect {|c| "#{c.name} - #{c.show_id.name}"}
   end
 end
